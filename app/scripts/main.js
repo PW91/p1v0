5b53708a0b885e4799630d9ded87dd2cbfd62cec
Przemyslaw Wieteska <wieteska.przemyslaw@gmail.com>
2018-03-12 11:13:52 +0100

    almost done

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,22 @@
 var sourceEl = document.querySelector('video source');
+var videoEl = document.querySelector('video');
 
-if (window.innerWidth < 768) {
+if (window.innerWidth <= 767) {
     sourceEl.setAttribute('src', './videos/mobile.mp4');
+    videoEl.setAttribute('poster', './images/poster_mobile.png');
 }
+
+window.lastWindowInnerWidth = window.innerWidth;
+
+window.addEventListener('resize', function() {
+
+    if (window.lastWindowInnerWidth > 767) {
+        if (window.innerWidth <= 767 ) {
+            window.location.reload();
+        }
+    } else {
+        if (window.innerWidth > 767 ) {
+            window.location.reload();
+        }
+    }
+});
